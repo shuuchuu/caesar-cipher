c4ebf8c415e85582d084617b97e8da40e2e2282a
mog <mog@noreply.git.ogion.eu>
2025-06-05 19:32:07 +0200

    Add solution
    Signed-off-by: mog <mog@noreply.git.ogion.eu>

--- src/caesar_cipher/caesar_cipher.py
+++ src/caesar_cipher/caesar_cipher.py
@@ -1,11 +1,14 @@
 from string import ascii_lowercase, ascii_uppercase
 
+import deal
+
 
 def make_table(characters: str, key: int) -> dict[int, int]:
     key %= 26
     return str.maketrans(characters, characters[key:] + characters[:key])
 
 
+@deal.ensure(lambda text, key, result: len(text) == len(result))
 def rotate(text: str, key: int) -> str:
     table = {**make_table(ascii_lowercase, key), **make_table(ascii_uppercase, key)}
     return text.translate(table)
